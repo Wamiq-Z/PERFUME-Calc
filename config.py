b123ccb78f81788d6f3aa9bc3a790b10519440cc
Zafar <wamiq.zafar@siemens.com>
2025-09-01 19:03:08 +0530

    latest config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:mzbrH7n3UCucEqFa@cahtntfghqaepqubnaua.supabase.co:5432/postgres"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres.cahtntfghqaepqubnaua:lavjain91405@aws-1-ap-south-1.pooler.supabase.com:5432/postgres"
 # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:wafzafar@localhost:5432/crud"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] =False 
 
