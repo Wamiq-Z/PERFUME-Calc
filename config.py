3a6dd3b6a24b85d4ad7b8a20a50d8d90c4bd4b52
Zafar <wamiq.zafar@siemens.com>
2025-09-02 19:17:12 +0530

    secured config

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres.cahtntfghqaepqubnaua:lavjain91405@aws-1-ap-south-1.pooler.supabase.com:5432/postgres"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SUPABASE_URI")
+print(app.config["SQLALCHEMY_DATABASE_URI"])
 # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:wafzafar@localhost:5432/crud"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] =False 
 
-app.secret_key = "supersecret123"
+app.secret_key = os.environ.get("SECRET")
 
 db = SQLAlchemy(app)
 
